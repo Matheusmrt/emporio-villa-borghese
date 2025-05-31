5909d40eb6cd2ecee0e2c3eeedb0a1f986aedc24
Matheusmrt <matecommrt@gmail.com>
2025-05-31 17:56:50 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
-// App component
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Empório Villa Borghese</h1>
+      <p>Projeto funcionando com Vite + React</p>
+    </div>
+  );
+}
+
+export default App;
