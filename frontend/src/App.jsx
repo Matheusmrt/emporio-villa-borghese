96ca66e54e76ec77405dceef3cc1ce409f1bc01b
Matheusmrt <matecommrt@gmail.com>
2025-06-01 22:54:25 -0300

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,22 @@
-/* App.jsx */
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Pedido from "./pages/Pedido";
+import Relatorio from "./pages/Relatorio";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/pedido" element={<Pedido />} />
+        <Route path="/relatorio" element={<Relatorio />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
